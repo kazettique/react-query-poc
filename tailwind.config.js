f77d949fc9cfa3aab5f9b683f0cc2d0f3fe2dbd1
Woody <kazettique@gmail.com>
2021-07-26 22:22:47 +0800

    Setup tailwind, path alias

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      height: ['hover', 'focus'],
+    },
+  },
+  plugins: [],
+};
